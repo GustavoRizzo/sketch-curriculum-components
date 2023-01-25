9b0ee30aa4a8f5b47bb3e899de5adfff71477655
GustavoRizzo <grizzo.albu@gmail.com>
2023-01-25 20:37:51 -0300

    test deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h1>Welcome!</h1>
-        <h2>Click on top menu to see the components</h2>
+        <h2>Click on top menu to see the components.</h2>
       </main>
     );
   }
