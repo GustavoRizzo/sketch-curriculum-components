f04393b9c74a6642ded3017b7ac3fcbfb37fb182
Gustavo Rizzo Albuquerque <grizzo.albu@gmail.com>
2023-07-09 20:34:59 -0300

    Making a button to hide some buttons of head menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,31 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import './style.scss'
 
 export default function Navbar() {
     const base = import.meta.env.BASE_URL;
+
+    const [isHidden, setIsHidden] = useState(false);
+
+    const toggleVisibility = () => {
+      setIsHidden(!isHidden);
+    };
+    
     return (
         <nav className="navbar">
             <Link to={base} className="navbar__title">Sketch Curriculum Components</Link>
-            <ul className="navbar__options">
-                <CustomLink to={`${base}ListExpertises`}>List Expertise</CustomLink>
-                <CustomLink to={`${base}About`}>About</CustomLink>
-                <CustomLink to={`${base}CareerTimeline`}>CareerTimeline</CustomLink>
-            </ul>
+            <button onClick={toggleVisibility} className="navbar__toggle-button">
+                <span role="img" aria-label="Hide/Show">
+                👁️
+                </span>
+            </button>
+            {!isHidden && (
+                <ul className="navbar__options">
+                    <CustomLink to={`${base}ListExpertises`}>List Expertise</CustomLink>
+                    <CustomLink to={`${base}About`}>About</CustomLink>
+                    <CustomLink to={`${base}CareerTimeline`}>CareerTimeline</CustomLink>
+                </ul>
+            )}
         </nav>
     );
 }
