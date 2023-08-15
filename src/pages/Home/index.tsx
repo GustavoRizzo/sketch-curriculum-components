683a8591f27f27810c30a57ac78881da8d722a23
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2023-08-14 23:57:21 -0300

    Clena code pages

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import About from "../About";
+import CareerTimeline from "../CareerTimeline";
+import ListExpertises from "../ListExpertises";
+
+export default function Home() {
+    return (
+      <main>
+        <About />
+        <ListExpertises />
+        <CareerTimeline />
+      </main>
+    );
+  }
