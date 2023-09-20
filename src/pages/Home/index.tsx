bc19d79f6b7fa4b41dafd144b49afab9c4d97d75
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2023-09-19 22:52:01 -0300

    Hiding LanguageSelector

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import LanguageSelector from "../../components/LanguageSelector/LanguageSelector";
 import About from "../About";
 import CareerTimeline from "../CareerTimeline";
 import ListExpertises from "../ListExpertises";
@@ -6,7 +5,6 @@ import ListExpertises from "../ListExpertises";
 export default function Home() {
     return (
       <main style={{padding: '0 1rem'}}>
-        <LanguageSelector />
         <About />
         <ListExpertises />
         <CareerTimeline />
